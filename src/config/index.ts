3b3ac0a9ce520937b3175d1febf06aaec0715abb
Timenakrab <timenakrab@gmail.com>
2023-03-19 03:46:22 +0700

    Fixed config environment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,19 @@
 const ENVIRONMENT: any = {
 	local: {
 		REACT_APP_NODE_ENVIRONMENT: 'local',
-		REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? '',
+		REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? 'https://pokeapi.co/api/v2',
 	},
 	development: {
 		REACT_APP_NODE_ENVIRONMENT: 'development',
-		REACT_APP_BACKEND_URL: '',
+		REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? 'https://pokeapi.co/api/v2',
 	},
 	qa: {
 		REACT_APP_NODE_ENVIRONMENT: 'qa',
-		REACT_APP_BACKEND_URL: '',
+		REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? 'https://pokeapi.co/api/v2',
 	},
 	production: {
 		REACT_APP_NODE_ENVIRONMENT: 'production',
-		REACT_APP_BACKEND_URL: '',
+		REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? 'https://pokeapi.co/api/v2',
 	},
 };
 export enum CONFIG_VALUE {
